b65c40863c4b2a5d4100e1fc9100380a49df06b6
Saan Saechao <saan@saechao.fyi>
2025-05-20 14:52:18 -0700

    updated datadog.tf to pass only correct parameters

--- prod/11-datadog.tf
+++ prod/11-datadog.tf
@@ -2,21 +2,20 @@ resource "aws_cloudformation_stack" "DatadogIntegration" {
   name = "DatadogIntegration"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters = {
-    APIKey = var.datadog_api_key
-    APPKey = var.datadog_application_key
-    DatadogSite = var.datadog_region
-    InstallLambdaLogForwarder = true
+    ExternalId = var.datadog_external_id
     IAMRoleName = "DatadogIntegrationRole"
-    CloudSecurityPostureManagement = false
-    DisableMetricCollection = false
-    ExternalId = var.datadog_external_id   # <-- # Added datadog_external_id variable needed to set the External ID for the Datadog AWS integration role
+    # Optionally add these if you want to override defaults:
+    # BasePermissions = "Full"
+    # LogArchives = ""
+    # CloudTrails = ""
+    # CloudSecurityPostureManagementPermissions = false
   }
   template_url = "https://${module.s3_bucket_for_output_files.s3_bucket_id}.s3.amazonaws.com/datadog_integration_role.yaml"
 
   lifecycle {
     ignore_changes = [
-      parameters["APIKey"],
-      parameters["APPKey"]
+      parameters["ExternalId"],
+      parameters["IAMRoleName"]
     ]
   }
 }
